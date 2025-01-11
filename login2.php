bed1ab5dcb2e9e78f0f334562c12df994e1990db
Jojococo2 <ummesalmalokhandwala33@gmail.com>
2025-01-11 14:45:16 +0530

    Update login2.php

--- login2.php
+++ login2.php
@@ -8,16 +8,9 @@
     $dbname="umme";
 
     $conn=mysqli_connect($servername,$username,$password,$dbname);
-    if($conn){
-        echo"connected";
-    }
-    /*$q1=mysqli_query($conn,"create table logins(Email varchar(30),Mobile varchar(30),Password varchar(30))");
-    if($q1){
-        echo" one connected";
-    }*/
+$q1=mysqli_query($conn,"create table logins(Email varchar(30),Mobile varchar(30),Password varchar(30))");
+
     $q2=mysqli_query($conn,"insert into logins(Email,Mobile,Password)values('$email','$mobile','$pass')");
-    if($q2){
-        echo" one";
-    }
+
 
     ?>
